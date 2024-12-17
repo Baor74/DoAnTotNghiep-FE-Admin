d71f3a1030621a31ac74885d1cf92331b5b72f15
Baor74 <nvhbao1132@gmail.com>
2024-12-17 16:16:36 +0700

    fixed feat login

--- app/components/login/login.js
+++ app/components/login/login.js
@@ -21,9 +21,12 @@ app.controller('LoginController', function ($scope, $location, $http, $window) {
 
                     const token = response.data.data.token;
                     const userId = response.data.data.userId;
+                    const roleName = response.data.data.roleName
+
                     localStorage.setItem('token', token);
                     localStorage.setItem('username', $scope.login.username);
                     localStorage.setItem('userId', userId);
+                    localStorage.setItem('roleName', roleName);
                     const redirectUrl = localStorage.getItem('redirectUrl');
                     if (redirectUrl) {
                         // Xóa URL đã lưu sau khi chuyển hướng
