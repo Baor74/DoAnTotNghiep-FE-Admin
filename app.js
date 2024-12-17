51e8c83f60e3b5b9c44f93b83e7b9eb45bdf94b4
Nguyen Duc Hao <haoxaomitom@gmail.com>
2024-12-17 20:46:44 +0700

    fix bug after merge bao to hao

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Khai báo module chính
 let app = angular.module('ParkingAdminApp', ['ngRoute', 'ngSanitize']);
 
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider,  $locationProvider) {
@@ -16,6 +15,30 @@ app.config(["$routeProvider", "$locationProvider", function ($routeProvider,  $l
         .when('/post', {
             templateUrl: 'app/components/admin/Post.html',
             controller: 'PostController'
+        })
+        .when('/post-detail', {
+            templateUrl: 'app/components/admin/PostDetail.html',
+            controller: 'PostDetailController'
+        })
+        .when('/approve', {
+            templateUrl: 'app/components/admin/AprovePost.html',
+            controller: 'ApprovalPostController'
+        })
+        .when('/account', {
+            templateUrl: 'app/components/admin/Account.html',
+            controller: 'AccountController'
+        })
+        .when('/payment', {
+            templateUrl: 'app/components/admin/Payment.html',
+            controller: 'PaymentController'
+        })
+        .when('/price', {
+            templateUrl: 'app/components/admin/Price.html',
+            controller: 'PriceController'
+        })
+        .when('/report', {
+            templateUrl: 'app/components/admin/Report.html',
+            controller: 'ReportController'
         })
         .when('/user', {
             templateUrl: 'app/components/admin/Users.html',
